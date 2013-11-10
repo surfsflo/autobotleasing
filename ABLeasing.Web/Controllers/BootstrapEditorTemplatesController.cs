37f0c0be461f14b3b11d5414e4834c2b18b2852a
John Walker <john.walker.dev@gmail.com>
2013-11-10 15:15:26 -0700

    starting on sign up controllers. just need to figure out how they work with the register model

--- ABLeasing.Web/Controllers/BootstrapEditorTemplatesController.cs
+++ ABLeasing.Web/Controllers/BootstrapEditorTemplatesController.cs
@@ -1,9 +1,13 @@
 using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
 
 namespace ABLeasing.Web.Controllers
 {
+    [RoutePrefix("Test")]
     public class BootstrapEditorTemplatesController : Controller
     {
+        [GET("")]
         public ActionResult Index()
         {
             return View();
