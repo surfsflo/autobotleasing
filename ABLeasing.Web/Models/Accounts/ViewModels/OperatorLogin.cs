35d5501a6f3356300e95f5426b280b2daa6fac71
John Walker <john.walker.dev@gmail.com>
2013-11-11 01:21:01 -0700

    member login and register working correctly now

---
+++ ABLeasing.Web/Models/Accounts/ViewModels/OperatorLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABLeasing.Web.Models.Accounts.ViewModels
+{
+    public class OperatorLogin
+    {
+        public Operator Operator { get; set; }
+        public RegisterModel RegisterModel { get; set; }
+    }
+}
