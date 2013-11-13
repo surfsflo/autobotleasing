f6083f467dd5ca27e5613a751751c2ca721349ad
John Walker <john.walker.dev@gmail.com>
2013-11-13 09:42:45 -0700

    separated out the login for the two modules, admin and app. modified the sass layout for the same reason. Working on creating admin accounts and the Status error in user profile creation.

---
+++ ABLeasing.Web/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using ABLeasing.Web.Models;
+using WebMatrix.WebData;
+
+namespace ABLeasing.Web.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [RoutePrefix("Account")]
+    public class AccountController : Controller
+    {
+
+        [GET("login")]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [ValidateAntiForgeryToken]
+        [POST("login")]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid && WebSecurity.Login(model.Email, model.Password, persistCookie: model.RememberMe))
+            {
+                return RedirectToLocal(returnUrl);
+            }
+
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            return View(model);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Index");
+            }
+        }
+
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+        }
+
+    }
+}
