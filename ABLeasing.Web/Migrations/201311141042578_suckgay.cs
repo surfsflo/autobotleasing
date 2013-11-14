34638f69692a0ace2977820255d7545a0fa92307
John Walker <john.walker.dev@gmail.com>
2013-11-14 03:53:43 -0700

    that was way to hard

---
+++ ABLeasing.Web/Migrations/201311141042578_suckgay.cs
@@ -0,0 +1,16 @@
+namespace ABLeasing.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class suckgay : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
--- ABLeasing.Web/Models/Accounts/Operator.cs
+++ ABLeasing.Web/Models/Accounts/Operator.cs
@@ -7,7 +7,7 @@ namespace ABLeasing.Web.Models.Accounts
 {
     public class Operator : UserProfile
     {
-        public int Rating { get; set; }
+
         // what is a researcher
         public string TypeOfBusiness { get; set; }
         public string Description { get; set; }
