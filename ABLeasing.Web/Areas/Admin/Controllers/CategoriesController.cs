c4622f3f9f857ba443e7bbdc6b717234b8ad7970
John Walker <john.walker.dev@gmail.com>
2013-11-13 22:32:10 -0700

    admin related fun. updated glimpse

---
+++ ABLeasing.Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using ABLeasing.Web.Models;
+using ABLeasing.Web.Infrastructure;
+
+namespace ABLeasing.Web.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    [RoutePrefix("Categories")]
+    public class CategoriesController : Controller
+    {
+        private ABLeasingDB db = new ABLeasingDB();
+
+        [GET("")]
+        public ActionResult Index()
+        {
+            return PartialView(db.Categories.ToList());
+        }
+
+        [GET("Details/{id}")]
+        public ActionResult Details(int id = 0)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        [GET("Create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [POST("Create")]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        [GET("Edit/{id}")]
+        public ActionResult Edit(int id = 0)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        [POST("Edit/{id}")]
+        public ActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [GET("Delete/{id}")]
+        public ActionResult Delete(int id = 0)
+        {
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        [POST("Delete/{id}")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
